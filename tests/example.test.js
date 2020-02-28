eba9c984923858e35cb4c48f38c552ee92b3890a
Kyle Brandt <kbrandt@wilsonelectronics.com>
2020-02-28 11:04:50 -0700

    Section 7: part 3 Done

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,13 +1,16 @@
 import Homepage from '../pages/HomePage';
+import LoginPage from '../pages/LoginPage';
 import TopBar from '../pages/componets/TopBar';
 
 describe('example', () => {
 	let homepage;
 	let topbar;
+	let loginpage;
 
 	beforeAll(async () => {
 		jest.setTimeout(20000);
 		homepage = new Homepage();
+		loginpage = new LoginPage();
 		topbar = new TopBar();
 	});
 
@@ -18,5 +21,11 @@ describe('example', () => {
 	it('navbar should be displayed', async () => {
 		await homepage.isNavbarDisplayed();
 		await topbar.isTopBarDisplayed();
+	});
+	it('Try to login', async () => {
+		await loginpage.visit();
+		await loginpage.isLoginFormDisplayed();
+		await loginpage.login('username', 'password');
+		await loginpage.wait(5000);
 	});
 });
