67af696ae93383c9eb065b1a540b13401c1e9a83
Kyle Brandt <kylebrandt9@gmail.com>
2020-03-02 17:15:30 -0700

    Section 5: Part 4 Done

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,16 +1,19 @@
 import Homepage from '../pages/HomePage';
 import LoginPage from '../pages/LoginPage';
+import FeedbackPage from '../pages/FeedbackPage';
 import TopBar from '../pages/componets/TopBar';
 
 describe('example', () => {
 	let homepage;
 	let topbar;
 	let loginpage;
+	let feedbackpage;
 
 	beforeAll(async () => {
 		jest.setTimeout(20000);
 		homepage = new Homepage();
 		loginpage = new LoginPage();
+		feedbackpage = new FeedbackPage();
 		topbar = new TopBar();
 	});
 
@@ -22,10 +25,23 @@ describe('example', () => {
 		await homepage.isNavbarDisplayed();
 		await topbar.isTopBarDisplayed();
 	});
+	/*
 	it('Try to login', async () => {
 		await loginpage.visit();
 		await loginpage.isLoginFormDisplayed();
 		await loginpage.login('username', 'password');
 		await loginpage.wait(5000);
+	});
+	*/
+	it('FeedBack should work', async () => {
+		await feedbackpage.visit();
+		await feedbackpage.isFeedbackFormDisplayed();
+		await feedbackpage.submitFeedback(
+			'John',
+			'test@msn.com',
+			'testing Subject',
+			'Test Comment'
+		);
+		await feedbackpage.wait(5000);
 	});
 });
